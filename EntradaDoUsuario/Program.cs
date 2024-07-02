7341841fbc7e8f403b2fb61802578ecae64b9cff
unknown <angelorequenha@gmail.com>
2024-07-02 13:37:25 -0300

    Entrada do usuário

---
+++ EntradaDoUsuario/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace EntradaDoUsuario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = "";
+            int num1 = 0;
+            int num2 = 0;
+
+
+            Console.WriteLine("Digite seu nome:");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("Digte um numero:");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite outro numero:");
+            num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Pois é {nome}, {num1}+{num2} não é {num1 - num2}");
+        }
+    }
+}
