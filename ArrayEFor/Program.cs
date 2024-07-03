b722d844de41b85a4118d4f4112b9329649498e0
unknown <angelorequenha@gmail.com>
2024-07-03 12:12:05 -0300

    For e foreach

--- ArrayEFor/Program.cs
+++ ArrayEFor/Program.cs
@@ -5,27 +5,46 @@
         static void Main(string[] args)
         {
             string[] produtos1 = new string[5] { // Pode definir a quantidade de itens
-                "Produto1",
+                "Deletado",
                 "Produto2",
                 "Produto3",
                 "Produto4",
                 "Produto5"
             };
 
-            string[] produtos2 = { // Define a quantidade de itens baseado em quantos itens tem
-                "Produto1",
-                "Produto2",
-                "Produto3",
-                "Produto4",
-                "Produto5"
+            int[] produtos2 = { // Define a quantidade de itens baseado em quantos itens tem
+                1,
+                2,
+                3,
+                4,
+                5
             };
 
             Console.WriteLine(produtos1[0]);
-            produtos1[0] = "Deletado";
+            produtos1[0] = "Produto1";
             Console.WriteLine(produtos1[0]);
 
-            
+            Console.WriteLine("------------------------");
+
+            foreach (string t in produtos1) {  // For para arrays
+                Console.WriteLine(t); 
             }
+
+            Console.WriteLine("------------------------");
+
+            for (int contador = 0; contador < produtos1.Length; contador++) {
+                Console.Write($"{produtos2[contador]} - ");
+                Console.WriteLine($"Produto{contador + 1}");
+            }
+
+            Console.WriteLine("------------------------");
+
+            for (int contador = produtos2.Length - 1; contador >= 0 ; contador--)
+            {
+                Console.Write($"{produtos2[contador]} - ");
+                Console.WriteLine($"Produto{contador + 1}");
+            }
+
         }
     }
 }
--- ArrayEFor/obj/Debug/net8.0/ArrayEFor.AssemblyInfo.cs
+++ ArrayEFor/obj/Debug/net8.0/ArrayEFor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArrayEFor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56cb68c9b00ad038d51995244898f9cc9bc546f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab8da3c0cc0fe5a350232e37b56c7f588b76331d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArrayEFor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArrayEFor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
