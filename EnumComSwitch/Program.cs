f01df9c1a788a287d803e3a9b55c3e43e3503ce1
unknown <angelorequenha@gmail.com>
2024-07-03 02:19:28 -0300

    EnumComSwitch

---
+++ EnumComSwitch/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace EnumComSwitch
+{
+    internal class Program
+    {
+        enum Opcao { Criar = 1, Deletar, Editar, Listar, Atualizar }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Selecione uma das opções abaixo:");
+            Console.WriteLine("1-Criar\n2-Deletar\n3-Editar\n4-Listar\n5-Atualizar");
+            int index = int.Parse(Console.ReadLine());
+            Opcao opcaoSelecionada = (Opcao)index;
+
+            switch (opcaoSelecionada)
+            {
+                case Opcao.Criar:
+                    Console.WriteLine("Você quer: Criar");
+                    break;
+                case Opcao.Deletar:
+                    Console.WriteLine("Você quer: Deletar");
+                    break;
+                case Opcao.Editar:
+                    Console.WriteLine("Você quer: Editar");
+                    break;
+                case Opcao.Listar:
+                    Console.WriteLine("Você quer: Listar");
+                    break;
+                case Opcao.Atualizar:
+                    Console.WriteLine("Você quer: Atualizar");
+                    break;
+                default:
+                    Console.WriteLine("Opção não encontrada");
+                    break;
+            }
+
+
+        }
+    }
+}
