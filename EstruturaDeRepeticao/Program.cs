ab8da3c0cc0fe5a350232e37b56c7f588b76331d
unknown <angelorequenha@gmail.com>
2024-07-03 11:02:38 -0300

    Estrutura de Repetição While

---
+++ EstruturaDeRepeticao/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace EstruturaDeRepeticao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 0;
+
+
+            while (contador < 10)
+            {
+                // contador = contador + 1
+                // contador += 1
+                contador++;
+                Console.WriteLine(contador);
+            }
+
+            contador = 0;
+
+            do {
+                contador++;
+                Console.WriteLine(contador);
+            } while (contador < 10);
+        }
+    }
+}
