39289c591cab8ce6baf4cfc909edf96e3c8a7d85
unknown <angelorequenha@gmail.com>
2024-07-03 12:52:48 -0300

    Chamando classes

---
+++ ChamandoClasses/Pessoa.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChamandoClasses
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+        public Pessoa(string nome, int idade)
+        {
+            Nome = nome;
+            Idade = idade;
+        }
+
+        public string Exibir()
+        {
+            return $"Nome: {Nome} | Idade: {Idade}";
+        }
+
+    }
+}
---
+++ ChamandoClasses/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ChamandoClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa1 = new Pessoa("Angelo", 21);
+
+            Console.WriteLine(pessoa1.Exibir());
+        }
+    }
+}
